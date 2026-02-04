5a5c199949db30f0873a00a71cf3426c7deebbe9
Kellam Adams <kellamadams@gmail.com>
2026-02-04 10:21:04 -0800

    Initial Commit

---
+++ Squatch Sports WatchOS Companion Watch App/Squatch_Sports_WatchOS_CompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Squatch_Sports_WatchOS_CompanionApp.swift
+//  Squatch Sports WatchOS Companion Watch App
+//
+//  Created by Kellam Adams on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct Squatch_Sports_WatchOS_Companion_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Squatch Sports WatchOS Companion Watch AppTests/Squatch_Sports_WatchOS_Companion_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Squatch_Sports_WatchOS_Companion_Watch_AppTests.swift
+//  Squatch Sports WatchOS Companion Watch AppTests
+//
+//  Created by Kellam Adams on 2/4/26.
+//
+
+import Testing
+@testable import Squatch_Sports_WatchOS_Companion_Watch_App
+
+struct Squatch_Sports_WatchOS_Companion_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
